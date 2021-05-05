996fefbdf3765fe5c87398e2d7e955134047efe0
Chris Vazquez <chrisv22193@yahoo.com>
2021-05-05 15:42:51 -0700

    Made some further changes as well as added Julios implementation of the calendar

---
+++ SocialCalendar/app/src/main/java/com/example/socialcalendar/CustomCalendar.java
@@ -0,0 +1,205 @@
+package com.example.socialcalendar;
+
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TimeZone;
+
+public class CustomCalendar extends LinearLayout {
+    ImageButton NextButton,PreviousButton;
+    TextView CurrentDate;
+    GridView gridView;
+    private static final int MAX_CALENDAR_DAYS=42;
+    Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+    Context context;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy", Locale.ENGLISH);
+    SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM",Locale.ENGLISH);
+    SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy",Locale.ENGLISH);
+    SimpleDateFormat eventDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+
+    MyGridAdapter myGridAdapter;
+    AlertDialog alertDialog;
+    List<Date> dates = new ArrayList<>();
+    ArrayList<Events> eventsList = new ArrayList<>();
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public CustomCalendar(Context context) {
+        super(context);
+    }
+    public CustomCalendar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        InitializeLayout();
+        SetUpCalendar();
+
+        PreviousButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.add(Calendar.MONTH, -1);
+                SetUpCalendar();
+            }
+        });
+
+        NextButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.add(Calendar.MONTH, 1);
+                SetUpCalendar();
+            }
+        });
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setCancelable(true);
+                View addView = LayoutInflater.from(parent.getContext()).inflate(R.layout.add_newevent_layout, null);
+                EditText EventName = addView.findViewById(R.id.eventname);
+                TextView EventTime = addView.findViewById(R.id.eventtime);
+                ImageButton SetTime = addView.findViewById(R.id.seteventtime);
+                Button AddEvent = addView.findViewById(R.id.addevent);
+                SetTime.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Calendar calendar = Calendar.getInstance();
+                        int hours = calendar.get(Calendar.HOUR_OF_DAY);
+                        int minutes = calendar.get(Calendar.MINUTE);
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(addView.getContext(), R.style.Theme_AppCompat_Dialog
+                                , new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                Calendar c = Calendar.getInstance();
+                                c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                                c.set(Calendar.MINUTE, minute);
+                                c.setTimeZone(TimeZone.getDefault());
+                                SimpleDateFormat hformate = new SimpleDateFormat("K:mm a", Locale.ENGLISH);
+                                String event_Time = hformate.format(c.getTime());
+                                EventTime.setText(event_Time);
+                            }
+                        }, hours, minutes, false);
+                        timePickerDialog.show();
+                    }
+                });
+                final String date = eventDateFormat.format(dates.get(position));
+                final String month = monthFormat.format(dates.get(position));
+                final String year = yearFormat.format(dates.get(position));
+
+                AddEvent.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SaveEvent(EventName.getText().toString(), EventTime.getText().toString(), date, month, year);
+                        SetUpCalendar();
+                        alertDialog.dismiss();
+                    }
+                });
+
+                builder.setView(addView);
+                alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final String date = eventDateFormat.format(dates.get(position));
+                db.collection(date).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            for(QueryDocumentSnapshot document : task.getResult()) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                                builder.setCancelable(true);
+                                View addView = LayoutInflater.from(parent.getContext()).inflate(R.layout.show_events_rowlayout, null);
+                                TextView EventDate = addView.findViewById(R.id.eventdate);
+                                TextView EventName = addView.findViewById(R.id.eventname);
+                                TextView EventTime = addView.findViewById(R.id.eventtime);
+
+                                EventName.setText(document.getId());
+                                EventTime.setText(document.getData().toString());
+                                EventDate.setText(date);
+
+                                builder.setView(addView);
+                                alertDialog = builder.create();
+                                alertDialog.show();
+                            }
+                        }
+                    }
+                });
+                return true;
+            }
+        });
+
+    }
+
+    public CustomCalendar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+    private void SaveEvent(String event, String time, String date, String month, String year) {
+        Map<String, Object> Time = new HashMap<>();
+        Time.put("Time", time);
+        db.collection(date).document(event).set(Time);
+
+        Toast.makeText(context, "Event Saved", Toast.LENGTH_SHORT).show();
+    }
+
+    private void InitializeLayout() {
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.calendar_layout, this);
+        NextButton = view.findViewById(R.id.nextBtn);
+        PreviousButton = view.findViewById(R.id.previousBtn);
+        CurrentDate = view.findViewById(R.id.current_Date);
+        gridView = view.findViewById(R.id.gridView);
+    }
+
+    private void SetUpCalendar() {
+        String currentDate = dateFormat.format(calendar.getTime());
+        CurrentDate.setText(currentDate);
+        dates.clear();
+        Calendar monthCalendar = (Calendar)calendar.clone();
+        monthCalendar.set(Calendar.DAY_OF_MONTH,1);
+        int FirstDayOfMonth = monthCalendar.get(Calendar.DAY_OF_WEEK)-1;
+        monthCalendar.add(Calendar.DAY_OF_MONTH, -FirstDayOfMonth);
+
+        while(dates.size() < MAX_CALENDAR_DAYS) {
+            dates.add(monthCalendar.getTime());
+            monthCalendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        myGridAdapter = new MyGridAdapter(context, dates, calendar, eventsList);
+        gridView.setAdapter(myGridAdapter);
+    }
+}
--- SocialCalendar/app/src/main/java/com/example/socialcalendar/FourthFragment.java
+++ SocialCalendar/app/src/main/java/com/example/socialcalendar/FourthFragment.java
@@ -55,10 +55,15 @@ public class FourthFragment extends Fragment {
         }
     }
 
+    CustomCalendar customCalendar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fourth, container, false);
+        View v =  inflater.inflate(R.layout.fragment_fourth, container, false);
+
+        customCalendar = (CustomCalendar) v.findViewById(R.id.custom_calendar);
+
+        return v;
     }
 }
